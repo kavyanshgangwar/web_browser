b3e4f699464a544e102b865ec2378d3d33c8a94a
Kavyansh Gangwar <kavyanshgangwar.cse18@itbhu.ac.in>
2019-09-28 19:35:48 +0530

    created wbgraphics and wbsettings
    wbgraphics contain main graphic window and wbsettings contain settings of web browser

---
+++ wbsettings.py
@@ -0,0 +1,8 @@
+class Settings():
+    #a class to store settings of web browser
+    def __init__(self):
+        #initialize screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (255,255,255)
+        
